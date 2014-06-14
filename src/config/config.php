1dbdb7d607293eef4ea4a162a6ca2dcece42595c
David Myers <david.d.myers@gmail.com>
2014-06-14 15:24:45 -0500

    Removed info

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'client_email'		=> 'david.d.myers@gmail.com',//'YOUR_CLIENT_EMAIL',
-    'api_key'			=> 'api.c8c7178c29-1645',//'YOUR_API_KEY',
+    'client_email'		=> 'YOUR_CLIENT_EMAIL',
+    'api_key'			=> 'YOUR_API_KEY',
     'api_version'		=> 'v1' // Desired API version
 );
